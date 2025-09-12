0a18184102b99112c618e8915d735c9d622ac2f8
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-09-12 21:42:04 +0200

    started on prices logic

--- main.go
+++ main.go
@@ -8,14 +8,6 @@ func main() {
 
 	result := make(map[float64][]float64)
 
-	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
-	}
-
 	fmt.Println(result)
 
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,1 +1,26 @@
 package prices
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	job.TaxIncludedPrices = make(map[string]float64)
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+}
