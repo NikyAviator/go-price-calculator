49c633cc89dd7c3cd88b477e1bd2d2391abac1ef
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-09-15 19:27:11 +0200

    writes the struct to files

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -2,6 +2,7 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
@@ -29,4 +30,21 @@ func ReadLines(path string) ([]string, error) {
 	}
 	file.Close()
 	return lines, nil
+}
+
+func WriteJSONToFile(path string, data any) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.New("failed to create the file")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		file.Close()
+		return errors.New("failed to convert data to json")
+	}
+	file.Close()
+	return nil
+
 }
